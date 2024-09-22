110e629ee0881cca8c90f923908222d90696d4a3
Виктория <Viktoria.Pestova@urfu.me>
2024-09-22 21:21:56 +0500

    добавление функций js

---
+++ js/function.js
@@ -0,0 +1,14 @@
+function checkStringLength(str, maxLength) {
+  return str.length <= maxLength;
+}
+
+function isPalindrome(str) {
+  const normalizedStr = str.replaceAll(' ', '').toLowerCase();
+  let reversedStr = '';
+
+  for (let i = normalizedStr.length - 1; i >= 0; i--) {
+    reversedStr += normalizedStr[i];
+  }
+
+  return normalizedStr === reversedStr;
+}
